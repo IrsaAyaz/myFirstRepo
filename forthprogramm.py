7fef7db719955e6900dfe65158e40d53c6717543
Irsa Ayaz <irsaayaz.525@gmail.com>
2025-04-28 16:28:30 +0500

    Add files via upload

---
+++ forthprogramm.py
@@ -0,0 +1,6 @@
+weight = 65.5
+milkQuantity = 1.75
+temperature = 37.7
+print (weight)
+print (milkQuantity)
+print (temperature)
---
+++ thridprogramm.py
@@ -0,0 +1,5 @@
+fan_on = True
+fan_of = False
+
+print (fan_on)
+print (fan_of)
